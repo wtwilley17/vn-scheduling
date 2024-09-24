699a7b7016d245e0ca09434da904a456a0f0c482
wtwilley17@gmail.com <wtwilley17@gmail.com>
2024-09-24 06:16:57 +0000

    Created ServerModule1, Form1.RowTemplate2, Form1.RowTemplate1 and Form1; Edited settings

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,19 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+from datetime import datetime
+
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    # Any code you write here will run before the form opens.
+
+  def date_picker_1_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    pass
+
